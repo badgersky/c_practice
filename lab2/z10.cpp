bd35ffa4f7a0f280432ab04c8b2687afb6659ec4
badgersky <szymonek.ptasznik@gmail.com>
2023-11-14 15:07:16 +0100

    created function that changes value of one variable into another and vice versa using pointers

---
+++ lab2/z10.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#pragma warning (disable: 4996)
+
+void change(int* x, int* y);
+
+int main() 
+{
+	int x, y, ret;
+	printf("Enter two integers:\n");
+	ret = scanf("%d%d", &x, &y);
+	printf("Numbers before change: x = %d, y = %d\n", x, y);
+	change(&x, &y);
+	printf("Numbers after change: x = %d, y = %d\n", x, y);
+}
+
+void change(int* x, int* y) {
+	int z = *x;
+	*x = *y;
+	*y = z;
+}
