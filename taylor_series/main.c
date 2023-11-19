632de4aae8ed5b1e56b5ab961d333fc54b38f8a5
badgersky <szymon.pt4sznik@gmail.com>
2023-11-19 18:23:59 +0100

    taylor expansion series function

--- taylor_series/main.c
+++ taylor_series/main.c
@@ -6,10 +6,11 @@
 
 void validate_n_m(int const *np, int const *mp, int ret);
 int validate_a_b_epx(double *a, double *b, double const *eps, int ret);
+void series(double x, int m, double eps, int *condition);
 
 int main() {
-    int n, m, ret, ab_check;
-    double a, b, eps;
+    int n, m, ret, ab_check, condition;
+    double a, b, eps, dx, i;
 
     printf("Enter n, m:\n");  //m n input and validation
     ret = scanf("%d %d", &n, &m);
@@ -22,7 +23,13 @@ int main() {
         printf("Invalid input");
         exit(2);
     }
-    printf("%lf %lf", a, b);
+
+    dx = (b - a) / n;
+    i = a;
+    while (i <= b) {
+        series(i, m, eps, &condition);
+        i += dx;
+    }
 }
 
 void validate_n_m(int const *np, int const *mp, int ret) {
@@ -37,22 +44,42 @@ int validate_a_b_epx(double *a, double *b, double const *eps, int ret) {
     if (ret != 3) {
         return 0;
     }
-
     if (*a > *b) {
         double z = *b;
         *b = *a;
         *a = z;
     }
-
     if (fabs(*a + 1.) < err || fabs(*b - 1.) < err) {
         return 0;
     }
     if (fabs(*a - *b) < err) {
         return 0;
     }
-
     if (*eps <= err) {
         return 0;
     }
     return 1;
+}
+
+void series(double x, int m, double eps, int *condition) {
+    double strict_v = pow((1 - x), -3./2.);
+    int i = 1;
+    double prev = 1., res = 1., next;
+
+    while (1) {
+        if (i >= m) {
+            *condition = 1;
+            break;
+        }
+        if (fabs(strict_v - res) < eps) {
+            *condition = 2;
+            break;
+        }
+
+        next = prev * ((2. * i + 1.) / (2. * i));
+        prev = next;
+        res += next * pow(x, i);
+        i++;
+    }
+    printf("%lf\t%lf\t%lf\t%d\t%d\n", x, res, strict_v, i, *condition);
 }
