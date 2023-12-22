b8c92f2fdb92bf8461b6a840dc11460dceaa7963
Szymon <szymonek.ptasznik@gmail.com>
2023-12-22 13:59:05 +0100

    changed data filename

--- lab4/z21.c
+++ lab4/z21.c
@@ -6,7 +6,7 @@ int sum_below_diag(FILE *f, int n, int m, int a, int b);
 
 int main() {
     FILE *fd;
-    fd = fopen("dane2122", "r");
+    fd = fopen("dane21", "r");
     if (!fd) {
         printf("file error");
         exit(1);
