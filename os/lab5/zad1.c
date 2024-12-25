bb38e64daac600817a0d524552c6c858609194bb
szymon <szymon.pt4sznik@gmail.com>
2024-12-25 22:18:25 +0100

    printing thread and process id

---
+++ os/lab5/zad1.c
@@ -0,0 +1,21 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void* print_ids(void* args) {
+    printf("id wątku: %ld\n", pthread_self());
+    printf("id procesu: %d\n", getpid());
+}
+
+int main() {
+    pthread_t th_id1, th_id2;
+
+    pthread_create(&th_id1, NULL, &print_ids, NULL);
+    pthread_create(&th_id2, NULL, &print_ids, NULL);
+
+    pthread_join(th_id1, NULL);
+    pthread_join(th_id1, NULL);
+
+    return 0;
+}
